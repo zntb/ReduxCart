283faf7149a3cd49400289aa32a211daf98c85a5
zntb <zenetibi@yahoo.com>
2023-12-27 10:37:55 +0200

    Add solution to increase/decrease the contents of the cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -22,7 +22,7 @@ const cartSlice = createSlice({
         });
       } else {
         existingItem.quantity++;
-        // existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {

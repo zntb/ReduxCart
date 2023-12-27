9e6475910bc3b9c7a3b396a01e5bec37c6445f60
zntb <zenetibi@yahoo.com>
2023-12-27 16:24:29 +0200

    Finalizing the Fetching Logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +52,10 @@ export const sendCartData = (cart) => {
         'https://react-http-89353-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
